6688508c8b9b5e8954e1562a5d950df7bf836025
Jayraj <arajjayraj18@gmail.com>
2024-08-16 20:49:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="plate"></div>
+      <div className="cup">
+        <div className="top">
+          <div className="vapour">
+            <span style={{ animationDelay: "0.1s" }}></span>
+            <span style={{ animationDelay: "0.3s" }}></span>
+            <span style={{ animationDelay: "0.5s" }}></span>
+            <span style={{ animationDelay: "0.7s" }}></span>
+            <span style={{ animationDelay: "0.9s" }}></span>
+            <span style={{ animationDelay: "0.11s" }}></span>
+            <span style={{ animationDelay: "0.13s" }}></span>
+            <span style={{ animationDelay: "0.15s" }}></span>
+            <span style={{ animationDelay: "0.16s" }}></span>
+            <span style={{ animationDelay: "0.18s" }}></span>
+            <span style={{ animationDelay: "0.20s" }}></span>
+            <span style={{ animationDelay: "0.22s" }}></span>
+            <span style={{ animationDelay: "0.24s" }}></span>
+            <span style={{ animationDelay: "0.26s" }}></span>
+            <span style={{ animationDelay: "0.28s" }}></span>
+            <span style={{ animationDelay: "0.30s" }}></span>
+            <span style={{ animationDelay: "0.32s" }}></span>
+            <span style={{ animationDelay: "0.34s" }}></span>
+            <span style={{ animationDelay: "0.36s" }}></span>
+            <span style={{ animationDelay: "0.38s" }}></span>
+            
+          </div>
+          <div className="circle">
+            <div className="tea"></div>
+          </div>
+        </div>
+        <div className="handle"></div>
+      </div>
     </div>
   );
 }
